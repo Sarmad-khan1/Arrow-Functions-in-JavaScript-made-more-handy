beee11baafeb9e267301fefb4397a2118423ea0a
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-12-30 14:57:11 +0500

    Add files via upload

---
+++ arrowFunctions.js
@@ -0,0 +1,16 @@
+ //Arrow Function to Divide Numbers in JavaScript....
+<script>
+
+        const divided = (number1, number2) => {
+            return number1/number2;
+        }
+        console.log(divided(40,4));
+
+        //Arrow Functions Made more handy and short.....
+
+
+
+        const divisible = (number1, number2) => number1 / number2;
+        console.log(divisible(100,1000));
+
+</script>
